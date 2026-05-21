6d140aa03b6ba51c620b551a0a57ddac75a5240f
Aditya Shelar <your-github-email@example.com>
2026-05-21 15:29:28 +0530

    Initial commit: Weather AI Agent

---
+++ main.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+client = OpenAI()
+
+def get_weather(city : str):
+    url = f"http://wttr.in/{city.lower()}?format=%C+%t"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return f"The weather in {city} is {response.text}"
+    else:
+        return "Sorry, I couldn't fetch the weather information right now."
+
+def main():
+    user_query = input("> ")
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "user", "content": user_query}
+        ]
+    )
+
+    print(f"🤖: {response.choices[0].message.content}")
+
+print(get_weather("New York"))
